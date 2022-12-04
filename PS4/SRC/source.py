1c0ad05edc814be1430902d01820f54dd4b84b37
tranbaoanhh27 <tranbaoanhh27@gmail.com>
2022-12-04 12:49:36 +0700

    Complete PL-RESOLUTION implementation.

--- PS4/SRC/source.py
+++ PS4/SRC/source.py
@@ -1,7 +1,9 @@
 import copy
+from typing import List, Set
 
-def SORT(literals: list[str]):
-    result = copy.deepcopy(literals)
+
+def SORT_LITERALS(literalList: List[str]):
+    result = copy.deepcopy(literalList)
     for i in range(len(result)):
         for j in range(i + 1, len(result)):
             a = result[i].replace('-', '')
@@ -11,6 +13,14 @@ def SORT(literals: list[str]):
                 result[i] = result[j]
                 result[j] = temp
     return result
+
+
+def NEGATIVE(literal: str):
+    if '-' in literal:
+        return literal.replace('-', '')
+    else:
+        return '-' + literal
+
 
 class CLAUSE:
 
@@ -23,11 +33,10 @@ class CLAUSE:
     def NOT(self):
         clauses = []
         for literal in self.data:
-            if '-' in literal:
-                clauses.append(CLAUSE(literal.replace('-', '')))
-            else:
-                clauses.append(CLAUSE('-' + literal))
-        return set(clauses)
+            clauses.append(NEGATIVE(literal))
+        result = CLAUSE('')
+        result.data = set(clauses)
+        return result
 
     def IS_EMPTY(self):
         return len(self.data) == 0
@@ -35,7 +44,7 @@ class CLAUSE:
     def __repr__(self) -> str:
         if len(self.data) == 0:
             return '{}'
-        literals = SORT(list(self.data))
+        literals = SORT_LITERALS(list(self.data))
         return ' OR '.join(literals)
 
     def __eq__(self, __o: object) -> bool:
@@ -45,45 +54,114 @@ class CLAUSE:
         return hash(self.__repr__())
 
 
+def RESOLVABLE(a: CLAUSE, b: CLAUSE) -> bool:
+    ''' Chỉ resolve khi hai mệnh đề có duy nhất một cặp literal đối nhau '''
+    countOppositePair = 0
+    for literal in a.data:
+        if NEGATIVE(literal) in b.data:
+            countOppositePair = countOppositePair + 1
+    return countOppositePair == 1
+
+
 def PL_RESOLVE(a: CLAUSE, b: CLAUSE) -> set:
-    pass
+    if RESOLVABLE(a, b):
+        data = list(a.data.union(b.data))
+        for i in range(len(data)):
+            for j in range(i + 1, len(data)):
+                x = data[i]
+                y = data[j]
+                if x != 'removed' and y != 'removed' and NEGATIVE(x) == y:
+                    data[i] = 'removed'
+                    data[j] = 'removed'
+        while 'removed' in data:
+            data.remove('removed')
+        result = CLAUSE('')
+        result.data = set(data)
+        return set([result])
+    else:
+        return set([])
 
 
-def CONTAINS_EMPTY_CLAUSE(clauses: set[CLAUSE]):
+def CONTAINS_EMPTY_CLAUSE(clauses: Set[CLAUSE]):
     for clause in clauses:
         if clause.IS_EMPTY():
             return True
     return False
 
 
-def PL_RESOLUTION(KB: set[CLAUSE], alpha: CLAUSE) -> bool:
+def PL_RESOLUTION(KB: Set[CLAUSE], alpha: CLAUSE, debug_mode: bool = False):
+
+    outputs = []
+
     clauses = copy.deepcopy(KB)
-    clauses.union(alpha.NOT())
+    clauses.add(alpha.NOT())
+
+    if debug_mode:
+        print('initial clauses =', clauses)
+
     new = set()
+
+    loop = 0
     while (True):
-        for i in range(len(clauses)):
-            for j in range(len(clauses)) and i != j:
-                resolvents = PL_RESOLVE(clauses[i], clauses[j])
-                if CONTAINS_EMPTY_CLAUSE(resolvents):
-                    return True
-                new.union(resolvents)
+        temp = list(clauses)
+
+        newClauses = []
+
+        loop = loop + 1
+        if debug_mode:
+            print('loop #', loop)
+        for i in range(len(temp)):
+            for j in range(i + 1, len(temp)):
+                if RESOLVABLE(temp[i], temp[j]):
+                    resolvents = PL_RESOLVE(temp[i], temp[j])
+                    if debug_mode:
+                        print(
+                            'resolve (' + str(temp[i]) + ') and (' + str(temp[j]) + ') = ' + str(resolvents))
+                    if not resolvents.issubset(clauses):
+                        newClauses = newClauses + list(resolvents)
+                    new = new.union(resolvents)
+
+        outputs.append(len(set(newClauses)))
+        outputs = outputs + list(set(newClauses))
+
+        if CONTAINS_EMPTY_CLAUSE(new):
+            if debug_mode:
+                print('new contains empty clause: ', new)
+            outputs.append('YES')
+            return outputs
+
         if new.issubset(clauses):
-            return False
+            if debug_mode:
+                print('new is subset of clauses')
+                print('new =', new)
+                print('clauses =', clauses)
+            outputs.append('NO')
+            return outputs
+
         clauses = clauses.union(new)
-        
 
-x = CLAUSE('A OR -B')
-y = CLAUSE('-B OR A')
-z = CLAUSE('{}')
 
-a = x.NOT()
-b = y.NOT()
+def READ_INPUT(inputFilePath: str):
+    try:
+        with open(inputFilePath, 'r') as inputFile:
+            lines = inputFile.readlines()
+            alpha = CLAUSE(lines[0].replace('\n', ''))
+            numOfClauses = int(lines[1].replace('\n', ''))
+            knowledgeBase = set([CLAUSE(lines[i + 2].replace('\n', '')) for i in range(numOfClauses)])
+            return knowledgeBase, alpha
+    except:
+        return None
 
-print('x = ', x)
-print('y = ', y)
-print('z =', z)
-print('a = ', a)
-print('b = ', b)
 
-print(x == y)
-print(a == b)
+def WRITE_OUTPUT(outputs: list, outputFilePath: str):
+    with open(outputFilePath, 'w') as outputFile:
+        outputFile.writelines([str(output) + '\n' for output in outputs])
+
+
+def main():
+    inputs = READ_INPUT('./INPUT/input0.txt')
+    result = PL_RESOLUTION(inputs[0], inputs[1], debug_mode=True)
+    print(result)
+    WRITE_OUTPUT(result, './OUTPUT/output0.txt')
+
+main()
